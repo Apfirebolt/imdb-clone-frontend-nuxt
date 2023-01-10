639a68ae0df28863f34d3c1b980318e5a43cb4a7
Amit P <aspper20@gmail.com>
2023-01-10 14:26:14 +0530

    feat: added vercel builder

--- components/Header.vue
+++ components/Header.vue
@@ -228,18 +228,13 @@
           </a>
         </nav>
         <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-          <a
-            href="#"
+          <NuxtLink to="/login" 
             class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-          >
-            Sign in
-          </a>
-          <a
-            href="#"
+          >Sign In</NuxtLink>
+          
+          <NuxtLink to="/register"
             class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-          >
-            Sign up
-          </a>
+          > Sign Up</NuxtLink>
         </div>
       </div>
     </div>
--- store/movie/state.js
+++ store/movie/state.js
@@ -1,3 +1,3 @@
 export default () => ({
-  single: {},
+  single: null,
 });
